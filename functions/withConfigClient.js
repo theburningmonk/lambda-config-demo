2c325cf3b69322c9e1d1e5715998af3356ef618b
theburningmonk <theburningmonk@gmail.com>
2017-08-30 02:34:54 +0100

    - show example of using different time out with different config values

--- functions/withConfigClient.js
+++ functions/withConfigClient.js
@@ -3,11 +3,19 @@
 const co           = require('co');
 const configClient = require('../lib/configClient');
 
-const config = configClient.loadConfigs(
+const config1 = configClient.loadConfigs(
   [ "foo", "bar" ], 
   30000); // cache config values for 30s
 
+const config2 = configClient.loadConfigs(
+  [ "jet", "pack" ]); // default cache expiration is 3 mins
+
 module.exports.handler = co.wrap(function* (event, context, callback) {
-  let params = [ yield config.foo, yield config.bar ];
+  let params = [ 
+    yield config1.foo, 
+    yield config1.bar,
+    yield config2.jet,
+    yield config2.pack
+  ];
   callback(null, params);
 });
